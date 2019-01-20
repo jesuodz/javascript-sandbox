7c29fec0fccacc70f1e4edef7854f1a73250e5f6
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-01-20 16:00:27 -0400

    Add solution to reverseArrayInPlace

--- chapter_04/reverse-array.js
+++ chapter_04/reverse-array.js
@@ -1,13 +1,21 @@
 const reverseArray = (arr) => {
-	let arr2 = [];
-  	for (var i=arr.length-1; i>=0; i--) {
+  let arr2 = [];
+    for (var i=arr.length-1; i>=0; i--) {
       arr2.push(arr[i]);
     }
   return arr2;
 }
 
-const reverseArrayInPlace = (arr) => {
-	return arr.reverse();
+function reverseArrayInPlace(arr) {
+  const halfLength = Math.floor(arr.length / 2);
+  var rightSide = arr.slice(-halfLength);
+  var limit = halfLength - 1;
+  
+  for (var i = limit; i>=0; i--) {
+    var temp = arr[i];
+    arr[i] = rightSide[limit-i];
+    arr[arr.length-i-1] = temp;
+  }
 }
 
 console.log(reverseArray(["A", "B", "C"]));
