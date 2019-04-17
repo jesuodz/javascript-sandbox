c06d39e0f5012ff2ac4e5c5f5cb37a24356a0122
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-04-16 22:39:41 -0400

    Build village state

--- chapter_07/automaton.js
+++ chapter_07/automaton.js
@@ -1,5 +1,26 @@
 // Automaton.js - Mail delivery robot for a virtual world
 
+// Create village state
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map( p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
 // Build village
 function buildGraph(edges) {
   let graph = Object.create(null);
